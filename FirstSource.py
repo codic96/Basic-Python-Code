c74e0d128185994f93a42ff9c90b01410c32264b
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2021-10-27 14:21:16 +0530

    Add files via upload

---
+++ FirstSource.py
@@ -0,0 +1,12 @@
+''' Python program to add two numbers'''
+num1 = 12
+num2 = 122
+sum = num1+num2 #adding two numbers
+print('Sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
+''' Adding two number provided by user input'''
+
+number1 = input('First number:')
+number2 = input('Seconf Number:')
+sum1 = float(number1)+ float(number2)
+print('Sum of {0} and {1} is{2}' .format(number1,number2,sum1))
